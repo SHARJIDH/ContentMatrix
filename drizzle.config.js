98d8d4359149476ec409f3497c0713a70aa39ca1
SHARJIDH <sharjidh2003@gmail.com>
2024-09-05 17:44:27 +0530

    "first commit with gemini api integration and content generation"

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+  schema: "./utils/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://first_owner:DVLC9rFMzx2d@ep-restless-glade-a50921u2.us-east-2.aws.neon.tech/Ai_Content_generator?sslmode=require",
+  },
+};
